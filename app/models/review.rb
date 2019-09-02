654313bccb13d5acf7b2902705919659984ea21d
Clarissa Wong <clairew427@gmail.com>
2019-09-02 16:20:39 +0800

    restaurant seeds

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,4 +2,6 @@ class Review < ApplicationRecord
   belongs_to :provider
   belongs_to :service
   belongs_to :user
+  validates :title, presence: true
+  validates :rating, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5] }
 end
