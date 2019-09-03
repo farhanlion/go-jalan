4bb471cb8efd0557536e7e24de1791bf122b2284
Edwin Ting <edwintwx@gmail.com>
2019-09-03 16:30:15 +0800

    Upload video and images to review

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,5 @@ class Review < ApplicationRecord
   belongs_to :user
   validates :title, presence: true
   validates :rating, presence: true, inclusion: { in: [0, 1, 2, 3, 4, 5] }
+  has_many :review_photos
 end
