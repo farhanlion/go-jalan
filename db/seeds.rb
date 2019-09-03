b3114e8abc7bf8d7865845b02b7d5b0605aa3377
Clarissa Wong <clairew427@gmail.com>
2019-09-03 14:22:08 +0800

    activity seeds switched to provider, added images

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,6 @@ require 'json'
 require 'pry'
 
 Photo.destroy_all
-Service.destroy_all
 ProviderTag.destroy_all
 ProviderCategory.destroy_all
 Tag.destroy_all
@@ -29,30 +28,30 @@ category_array.each do |category|
   new_cat.save!
 end
 
-# Seed restaurants
-file_path = File.join(__dir__, 'restaurants.csv')
-counter = 1
-CSV.foreach(file_path, {:headers => true, :header_converters => :symbol}) do |row|
-  new_provider = Provider.new(name: row[:name], description: row[:description], open_hours: row[:hours], price: row[:price], country: 'Singapore')
-  new_provider_category = ProviderCategory.new(category: Category.find_by(name: 'Restaurants'), provider: new_provider)
-  new_provider_category.save!
-  row[:good_for].split(" ").each do |tag|
-    new_tag = Tag.new(name: tag)
-    new_tag.category = Category.find_by(name: 'Restaurants')
-    new_tag.save!
-    new_provider_tag = ProviderTag.new(tag: new_tag, provider: new_provider)
-    new_provider_tag.save!
-  end
-  row[:image].split(" ").each do |photo_url|
-    new_photo = Photo.new(provider: new_provider)
-    new_photo.remote_photo_url = photo_url
-    new_photo.save!
-  end
-  # row[:address].match(/(.*)(Singapore.*)/)
-  new_provider.save if Provider.find_by(name: row[:name]) == nil
-  counter += 1 if new_provider.save
-  break if counter > 10
-end
+# # Seed restaurants
+# file_path = File.join(__dir__, 'restaurants.csv')
+# counter = 1
+# CSV.foreach(file_path, {:headers => true, :header_converters => :symbol}) do |row|
+#   new_provider = Provider.new(name: row[:name], description: row[:description], open_hours: row[:hours], price: row[:price], country: 'Singapore')
+#   new_provider_category = ProviderCategory.new(category: Category.find_by(name: 'Restaurants'), provider: new_provider)
+#   new_provider_category.save!
+#   row[:good_for].split(" ").each do |tag|
+#     new_tag = Tag.new(name: tag)
+#     new_tag.category = Category.find_by(name: 'Restaurants')
+#     new_tag.save!
+#     new_provider_tag = ProviderTag.new(tag: new_tag, provider: new_provider)
+#     new_provider_tag.save!
+#   end
+#   row[:image].split(" ").each do |photo_url|
+#     new_photo = Photo.new(provider: new_provider)
+#     new_photo.remote_photo_url = photo_url
+#     new_photo.save!
+#   end
+#   # row[:address].match(/(.*)(Singapore.*)/)
+#   new_provider.save if Provider.find_by(name: row[:name]) == nil
+#   counter += 1 if new_provider.save
+#   break if counter > 10
+# end
 
 
 
@@ -62,21 +61,30 @@ viator_doc = Nokogiri::HTML(File.open(file_path), nil, 'utf-8')
 viator_doc.search(".product-card-main-content").each do |element|
   name = element.search("h2").text.strip
   price =  element.search(".h4").text.strip.split(" ").first
-  url = element.search("a").map do |element|
+  activity_url = element.search("a").map do |element|
     element.to_h["href"]
   end
-  activity_doc = Nokogiri::HTML(open(url[0]), nil, 'utf-8')
-  description =  activity_doc.search(".mb-5 .mb-3").text.strip.match(/Overview(.*)/)[1]
+  activity_page = Nokogiri::HTML(open(activity_url[0]), nil, 'utf-8')
+  description =  activity_page.search(".mb-5 .mb-3").text.strip.match(/Overview(.*)/)[1]
   country = 'Singapore'
 
-  new_provider = Provider.new(name: name)
+  image_url = []
+  activity_page.search(".hero-photo-overlay img").each do |image|
+    image_url << image.attributes["data-lazy"].value
+  end
+
+  p image_url
+
+  new_provider = Provider.new(name: name, description: description, country: country)
   new_provider.save!
+  image_url.each do |url|
+    new_photo = Photo.new(provider: new_provider)
+    new_photo.remote_photo_url = url
+    new_photo.save!
+  end
 
   new_provider_category = ProviderCategory.new(category: Category.find_by(name: 'Activities'), provider: new_provider)
   new_provider_category.save!
-
-  new_service = Service.new(name: name, description: description, country: country, provider: new_provider)
-  new_service.save!
 
   tag =  element.search(".category-card-tag").text
   new_tag = Tag.new(name: tag, category: Category.find_by(name: 'Activities'))
@@ -87,52 +95,52 @@ viator_doc.search(".product-card-main-content").each do |element|
 end
 
 
-# BEAUTY COMPANIES
+# # BEAUTY COMPANIES
 
-# parse beauty.json
-filepath = File.join(__dir__, 'beauty.json')
-searialised_beauty_places = File.read(filepath)
-beauty_places = JSON.parse(searialised_beauty_places)
-
-
-#create beauty companies
-beauty_tags = nil
-
-# create beauty companies
-beauty_places['beauty_companies'].each do |company|
-  beauty_tags = company['categories'].gsub("  ","").split(",")
-  new_company(company['name'], company['name'], company['description'], company['address'], company['phone'], company['website'])
-end
-#create beauty tags
-beauty_tags.uniq!
-beauty_tags.each do |tag|
-  new_tag = Tag.new(name: tag)
-  new_tag.category = Category.find_by(name: 'Beauty')
-  new_tag.save!
-end
-
-# FITNESS COMPANIES
-
-# parse fitness.json
-filepath = File.join(__dir__, 'fitness.json')
-searialised_fitness_places = File.read(filepath)
-fitness_places = JSON.parse(searialised_fitness_places)
+# # parse beauty.json
+# filepath = File.join(__dir__, 'beauty.json')
+# searialised_beauty_places = File.read(filepath)
+# beauty_places = JSON.parse(searialised_beauty_places)
 
 
-#create fitness companies
-fitness_tags = nil
-fitness_places['fitness_companies'].each do |company|
-  fitness_tags = company['categories'].gsub("  ","").split(",")
-  new_company(company['name'], company['name'], company['description'], company['address'], company['phone'], company['website'])
-end
+# #create beauty companies
+# beauty_tags = nil
 
-#create fitness tags
-fitness_tags.uniq!
-fitness_tags.each do |tag|
-  new_tag = Tag.new(name: tag)
-  new_tag.category = Category.find_by(name:'Fitness')
-  new_tag.save!
-end
+# # create beauty companies
+# beauty_places['beauty_companies'].each do |company|
+#   beauty_tags = company['categories'].gsub("  ","").split(",")
+#   new_company(company['name'], company['name'], company['description'], company['address'], company['phone'], company['website'])
+# end
+# # create beauty tags
+# beauty_tags.uniq!
+# beauty_tags.each do |tag|
+#   new_tag = Tag.new(name: tag)
+#   new_tag.category = Category.find_by(name: 'Beauty')
+#   new_tag.save!
+# end
+
+# # FITNESS COMPANIES
+
+# # parse fitness.json
+# filepath = File.join(__dir__, 'fitness.json')
+# searialised_fitness_places = File.read(filepath)
+# fitness_places = JSON.parse(searialised_fitness_places)
+
+
+# # create fitness companies
+# fitness_tags = nil
+# fitness_places['fitness_companies'].each do |company|
+#   fitness_tags = company['categories'].gsub("  ","").split(",")
+#   new_company(company['name'], company['name'], company['description'], company['address'], company['phone'], company['website'])
+# end
+
+# # create fitness tags
+# fitness_tags.uniq!
+# fitness_tags.each do |tag|
+#   new_tag = Tag.new(name: tag)
+#   new_tag.category = Category.find_by(name:'Fitness')
+#   new_tag.save!
+# end
 
 
 # def new_company(name, translated_name, description, address, phone_number, website)
