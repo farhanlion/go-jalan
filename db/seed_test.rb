66095253d8f9b7a03e857447da5bec00438b95eb
Clarissa Wong <clairew427@gmail.com>
2019-09-04 16:48:51 +0800

    added standardised address, lat and lng for resto

--- db/seed_test.rb
+++ db/seed_test.rb
@@ -1,6 +1,7 @@
 require 'Nokogiri'
 require 'pry'
 require 'open-uri'
+require 'csv'
 
 
 file_path = File.join(__dir__, 'viator.html')
@@ -10,6 +11,31 @@ viator_doc.search(".product-card-main-content").each do |element|
     element.to_h["href"]
   end
   activity_page = Nokogiri::HTML(open(activity_url[0]), nil, 'utf-8')
-  p activity_page.search(".collapse .show")
+   element = activity_page.search(".collapse .show")
+   element.each do |e|
+    p e
+    end
   break
 end
+
+# counter = 1
+# file_path = File.join(__dir__, 'restaurants.csv')
+# CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
+
+#   post_code = row[:address].match(/Singapore \((.*)\)/)[1]
+#   url = "https://developers.onemap.sg/commonapi/search?searchVal=#{post_code}&returnGeom=Y&getAddrDetails=Y&pageNum=1"
+#   response = open(url).read
+#   results = JSON.parse(response)["results"][0]
+#   if !results.empty?
+#     standardised_street_address = results["ADDRESS"]
+#     lat = results["LATITUDE"]
+#     lng = results["LONGITUDE"]
+#     p lat
+#     p lng
+# #   end
+
+
+
+#   counter += 1
+#   break if counter >= 10
+# end
