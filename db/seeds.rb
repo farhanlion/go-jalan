4852540697ec06a47c39382c06ecd23d7072159c
Farhan Ahsan <bbss1e3farhan@gmail.com>
2019-09-04 15:47:04 +0800

    latlong

--- db/seeds.rb
+++ db/seeds.rb
@@ -90,7 +90,7 @@ def url_should_be_accessible(url)
   success
 end
 
-# Seed 10 activities
+# Seed 10 activitiesHey
 puts 'Creating activities...'
 file_path = File.join(__dir__, 'viator.html')
 counter = 1
