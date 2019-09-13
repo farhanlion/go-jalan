aba5b6fe487f4af09b54e964f82907a5ae5d1e2e
Clarissa Wong <clairew427@gmail.com>
2019-09-13 11:13:57 +0800

    restaurant seeds fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,124 +20,124 @@ require 'net/http'
 # end
 
 # BEAUTY COMPANIES
-puts 'Creating beauty companies...'
+# puts 'Creating beauty companies...'
 
-filepath = File.join(__dir__, 'beauty.json')
-serialised_beauty_places = File.read(filepath)
-beauty_places = JSON.parse(serialised_beauty_places)
+# filepath = File.join(__dir__, 'beauty.json')
+# serialised_beauty_places = File.read(filepath)
+# beauty_places = JSON.parse(serialised_beauty_places)
 
-beauty_places['beauty_companies'].each do |company|
-puts Provider.count
+# beauty_places['beauty_companies'].each do |company|
+# puts Provider.count
 
-  new_provider = Provider.new(name: company['name'], description: company['description'], phone_number: company['phone'], country: 'Singapore')
-puts new_provider.name
+#   new_provider = Provider.new(name: company['name'], description: company['description'], phone_number: company['phone'], country: 'Singapore')
+# puts new_provider.name
 
-  post_code = company['address'].match(/(\d{6})/)
-  if post_code != nil
-  url = "https://developers.onemap.sg/commonapi/search?searchVal=#{post_code}&returnGeom=Y&getAddrDetails=Y&pageNum=1"
-  response = open(url).read
-  results = JSON.parse(response)["results"][0]
-  if !results.empty?
-    street_address = results["ADDRESS"]
-    latitude = results["LATITUDE"]
-    longitude = results["LONGITUDE"]
-  end
+#   post_code = company['address'].match(/(\d{6})/)
+#   if post_code != nil
+#   url = "https://developers.onemap.sg/commonapi/search?searchVal=#{post_code}&returnGeom=Y&getAddrDetails=Y&pageNum=1"
+#   response = open(url).read
+#   results = JSON.parse(response)["results"][0]
+#   if !results.empty?
+#     street_address = results["ADDRESS"]
+#     latitude = results["LATITUDE"]
+#     longitude = results["LONGITUDE"]
+#   end
 
-  new_provider.street_address = street_address
-  new_provider.latitude = latitude
-  new_provider.longitude = longitude
+#   new_provider.street_address = street_address
+#   new_provider.latitude = latitude
+#   new_provider.longitude = longitude
 
-  company['image'].each do |pic|
-    new_photo = Photo.new(provider: new_provider)
-    new_photo.remote_photo_url = pic
-    new_photo.save!
+#   company['image'].each do |pic|
+#     new_photo = Photo.new(provider: new_provider)
+#     new_photo.remote_photo_url = pic
+#     new_photo.save!
 
-  end
+#   end
 
-  new_provider_category = ProviderCategory.new(category: Category.find_by(name: 'Beauty'), provider: new_provider)
-  new_provider_category.save!
+#   new_provider_category = ProviderCategory.new(category: Category.find_by(name: 'Beauty'), provider: new_provider)
+#   new_provider_category.save!
 
-  company['tags'].each do |tag|
-    new_tag = Tag.new(name: tag)
-    if !Tag.all.include?(new_tag)
-      new_tag.category = Category.find_by(name: 'Beauty')
-      new_tag.save!
-      new_provider_tag = ProviderTag.new(tag: new_tag, provider: new_provider)
-      new_provider_tag.save!
-    else
-      new_provider_tag = ProviderTag.new(tag: Tag.find_by(name: tag), provider: new_provider)
-      new_provider_tag.save!
-    end
-  end
-end
-end
+#   company['tags'].each do |tag|
+#     new_tag = Tag.new(name: tag)
+#     if !Tag.all.include?(new_tag)
+#       new_tag.category = Category.find_by(name: 'Beauty')
+#       new_tag.save!
+#       new_provider_tag = ProviderTag.new(tag: new_tag, provider: new_provider)
+#       new_provider_tag.save!
+#     else
+#       new_provider_tag = ProviderTag.new(tag: Tag.find_by(name: tag), provider: new_provider)
+#       new_provider_tag.save!
+#     end
+#   end
+# end
+# end
 
 
 
 #FITNESS COMPANIES
 # frozen_string_literal: true
 
-puts 'Creating fitness companies...'
+# puts 'Creating fitness companies...'
 
-filepath = File.join(__dir__, 'fitness.json')
-serialised_fitness_places = File.read(filepath)
-byebug
-fitness_places = JSON.parse(serialised_fitness_places)
+# filepath = File.join(__dir__, 'fitness.json')
+# serialised_fitness_places = File.read(filepath)
+# byebug
+# fitness_places = JSON.parse(serialised_fitness_places)
 
-fitness_places['fitness_companies'].each do |company|
-  puts Provider.count
-  new_provider = Provider.new(name: company['name'], description: company['description'], phone_number: company['phone'], country: 'Singapore')
-  puts new_provider.name
-  post_code = company['address'].match(/(\d{6})/)
-  next if post_code.nil?
+# fitness_places['fitness_companies'].each do |company|
+#   puts Provider.count
+#   new_provider = Provider.new(name: company['name'], description: company['description'], phone_number: company['phone'], country: 'Singapore')
+#   puts new_provider.name
+#   post_code = company['address'].match(/(\d{6})/)
+#   next if post_code.nil?
 
-  url = "https://developers.onemap.sg/commonapi/search?searchVal=#{post_code}&returnGeom=Y&getAddrDetails=Y&pageNum=1"
-  response = open(url).read
-  results = JSON.parse(response)['results'][0]
-  unless results.empty?
-    street_address = results['ADDRESS']
-    latitude = results['LATITUDE']
-    longitude = results['LONGITUDE']
-  end
-
-
-    new_provider = Provider.new(name: name, description: description, price: price, country: country, latitude: 1.3521, longitude: 103.8198)
-    if Provider.find_by(name: name).nil?
-        new_provider.save!
-        image_urls.each do |url|
-          new_photo = Photo.new(provider: new_provider)
-          if url_should_be_accessible(url)
-            new_photo.remote_photo_url = url
-            new_photo.save!
-          end
-        end
-
-        new_provider_category = ProviderCategory.new(category: Category.find_by(name: 'Activities'), provider: new_provider)
-        new_provider_category.save!
-
-        tag = element.search('.category-card-tag').text
-        new_tag = Tag.new(name: tag)
-        if !Tag.all.include?(new_tag)
-          new_tag.category = Category.find_by(name: 'Restaurants')
-          new_tag.save!
-          new_provider_tag = ProviderTag.new(tag: new_tag, provider: new_provider)
-          new_provider_tag.save!
-        else
-          new_provider_tag = ProviderTag.new(tag: Tag.find_by(name: tag), provider: new_provider)
-          new_provider_tag.save!
-        end
-      end
-    end
-end
+#   url = "https://developers.onemap.sg/commonapi/search?searchVal=#{post_code}&returnGeom=Y&getAddrDetails=Y&pageNum=1"
+#   response = open(url).read
+#   results = JSON.parse(response)['results'][0]
+#   unless results.empty?
+#     street_address = results['ADDRESS']
+#     latitude = results['LATITUDE']
+#     longitude = results['LONGITUDE']
+#   end
 
 
-page_counter = 1
+#     new_provider = Provider.new(name: name, description: description, price: price, country: country, latitude: 1.3521, longitude: 103.8198)
+#     if Provider.find_by(name: name).nil?
+#         new_provider.save!
+#         image_urls.each do |url|
+#           new_photo = Photo.new(provider: new_provider)
+#           if url_should_be_accessible(url)
+#             new_photo.remote_photo_url = url
+#             new_photo.save!
+#           end
+#         end
 
-5.times do
-  file_path = File.join(__dir__, "viator#{page_counter}.html")
-  scrape_viator(file_path)
-  page_counter += 1
-end
+#         new_provider_category = ProviderCategory.new(category: Category.find_by(name: 'Activities'), provider: new_provider)
+#         new_provider_category.save!
+
+#         tag = element.search('.category-card-tag').text
+#         new_tag = Tag.new(name: tag)
+#         if !Tag.all.include?(new_tag)
+#           new_tag.category = Category.find_by(name: 'Restaurants')
+#           new_tag.save!
+#           new_provider_tag = ProviderTag.new(tag: new_tag, provider: new_provider)
+#           new_provider_tag.save!
+#         else
+#           new_provider_tag = ProviderTag.new(tag: Tag.find_by(name: tag), provider: new_provider)
+#           new_provider_tag.save!
+#         end
+#       end
+#     end
+# end
+
+
+# page_counter = 1
+
+# 5.times do
+#   file_path = File.join(__dir__, "viator#{page_counter}.html")
+#   scrape_viator(file_path)
+#   page_counter += 1
+# end
 
 
 # # Seed restaurants
@@ -211,72 +211,72 @@ end
 
 
 # BEAUTY COMPANIES
-puts 'Creating beauty companies...'
+# puts 'Creating beauty companies...'
 
-filepath = File.join(__dir__, 'beauty.json')
-serialised_beauty_places = File.read(filepath)
-beauty_places = JSON.parse(serialised_beauty_places)
+# filepath = File.join(__dir__, 'beauty.json')
+# serialised_beauty_places = File.read(filepath)
+# beauty_places = JSON.parse(serialised_beauty_places)
 
-beauty_places['beauty_companies'].each do |company|
-puts Provider.count
-byebug if Provider.count==109
+# beauty_places['beauty_companies'].each do |company|
+# puts Provider.count
+# byebug if Provider.count==109
 
-  new_provider = Provider.new(name: company['name'], description: company['description'], phone_number: company['phone'], country: 'Singapore')
-puts new_provider.name
+#   new_provider = Provider.new(name: company['name'], description: company['description'], phone_number: company['phone'], country: 'Singapore')
+# puts new_provider.name
 
-  post_code = company['address'].match(/(\d{6})/)
-  if post_code != nil
-  url = "https://developers.onemap.sg/commonapi/search?searchVal=#{post_code}&returnGeom=Y&getAddrDetails=Y&pageNum=1"
-  response = open(url).read
-  results = JSON.parse(response)["results"][0]
-  if !results.empty?
-    street_address = results["ADDRESS"]
-    latitude = results["LATITUDE"]
-    longitude = results["LONGITUDE"]
-  end
-
-
-  new_provider.street_address = street_address
-  new_provider.latitude = latitude
-  new_provider.longitude = longitude
-
-  company['image'].each do |pic|
-    new_photo = Photo.new(provider: new_provider)
-    new_photo.remote_photo_url = pic
-    new_photo.save!
-
-    p new_photo
-  end
-
-  new_provider_category = ProviderCategory.new(category: Category.find_by(name: 'Fitness'), provider: new_provider)
+#   post_code = company['address'].match(/(\d{6})/)
+#   if post_code != nil
+#   url = "https://developers.onemap.sg/commonapi/search?searchVal=#{post_code}&returnGeom=Y&getAddrDetails=Y&pageNum=1"
+#   response = open(url).read
+#   results = JSON.parse(response)["results"][0]
+#   if !results.empty?
+#     street_address = results["ADDRESS"]
+#     latitude = results["LATITUDE"]
+#     longitude = results["LONGITUDE"]
+#   end
 
 
-  end
+#   new_provider.street_address = street_address
+#   new_provider.latitude = latitude
+#   new_provider.longitude = longitude
 
-  new_provider_category = ProviderCategory.new(category: Category.find_by(name: 'Beauty'), provider: new_provider)
+#   company['image'].each do |pic|
+#     new_photo = Photo.new(provider: new_provider)
+#     new_photo.remote_photo_url = pic
+#     new_photo.save!
 
-  new_provider_category.save!
+#     p new_photo
+#   end
 
-  company['tags'].each do |tag|
-    new_tag = Tag.new(name: tag)
-    if !Tag.all.include?(new_tag)
-
+#   new_provider_category = ProviderCategory.new(category: Category.find_by(name: 'Fitness'), provider: new_provider)
 
 
-      new_tag.category = Category.find_by(name: 'Beauty')
-      new_tag.save!
-      new_provider_tag = ProviderTag.new(tag: new_tag, provider: new_provider)
-      new_provider_tag.save!
-      p new_provider_tag
-    else
-      new_provider_tag = ProviderTag.new(tag: Tag.find_by(name: tag), provider: new_provider)
-      new_provider_tag.save!
-      p new_provider_tag
-    end
-    new_provider.save!
-  end
-end
-end
+#   end
+
+#   new_provider_category = ProviderCategory.new(category: Category.find_by(name: 'Beauty'), provider: new_provider)
+
+#   new_provider_category.save!
+
+#   company['tags'].each do |tag|
+#     new_tag = Tag.new(name: tag)
+#     if !Tag.all.include?(new_tag)
+
+
+
+#       new_tag.category = Category.find_by(name: 'Beauty')
+#       new_tag.save!
+#       new_provider_tag = ProviderTag.new(tag: new_tag, provider: new_provider)
+#       new_provider_tag.save!
+#       p new_provider_tag
+#     else
+#       new_provider_tag = ProviderTag.new(tag: Tag.find_by(name: tag), provider: new_provider)
+#       new_provider_tag.save!
+#       p new_provider_tag
+#     end
+#     new_provider.save!
+#   end
+# end
+# end
 
 
 
@@ -357,62 +357,62 @@ end
 #   scrape_viator(file_path)
 #   page_counter += 1
 # end
-=======
+
 #FITNESS COMPANIES
 # frozen_string_literal: true
 
-puts 'Creating fitness companies...'
+# puts 'Creating fitness companies...'
 
-filepath = File.join(__dir__, 'fitness.json')
-serialised_fitness_places = File.read(filepath)
-byebug
-fitness_places = JSON.parse(serialised_fitness_places)
+# filepath = File.join(__dir__, 'fitness.json')
+# serialised_fitness_places = File.read(filepath)
+# byebug
+# fitness_places = JSON.parse(serialised_fitness_places)
 
-fitness_places['fitness_companies'].each do |company|
-  puts Provider.count
-  new_provider = Provider.new(name: company['name'], description: company['description'], phone_number: company['phone'], country: 'Singapore')
-  puts new_provider.name
-  post_code = company['address'].match(/(\d{6})/)
-  next if post_code.nil?
+# fitness_places['fitness_companies'].each do |company|
+#   puts Provider.count
+#   new_provider = Provider.new(name: company['name'], description: company['description'], phone_number: company['phone'], country: 'Singapore')
+#   puts new_provider.name
+#   post_code = company['address'].match(/(\d{6})/)
+#   next if post_code.nil?
 
-  url = "https://developers.onemap.sg/commonapi/search?searchVal=#{post_code}&returnGeom=Y&getAddrDetails=Y&pageNum=1"
-  response = open(url).read
-  results = JSON.parse(response)['results'][0]
-  unless results.empty?
-    street_address = results['ADDRESS']
-    latitude = results['LATITUDE']
-    longitude = results['LONGITUDE']
-  end
-  new_provider.street_address = street_address
-  new_provider.latitude = latitude
-  new_provider.longitude = longitude
+#   url = "https://developers.onemap.sg/commonapi/search?searchVal=#{post_code}&returnGeom=Y&getAddrDetails=Y&pageNum=1"
+#   response = open(url).read
+#   results = JSON.parse(response)['results'][0]
+#   unless results.empty?
+#     street_address = results['ADDRESS']
+#     latitude = results['LATITUDE']
+#     longitude = results['LONGITUDE']
+#   end
+#   new_provider.street_address = street_address
+#   new_provider.latitude = latitude
+#   new_provider.longitude = longitude
 
-  company['image'].each do |pic|
-    new_photo = Photo.new(provider: new_provider)
-    new_photo.remote_photo_url = pic
-    new_photo.save!
-    p new_photo
-  end
+#   company['image'].each do |pic|
+#     new_photo = Photo.new(provider: new_provider)
+#     new_photo.remote_photo_url = pic
+#     new_photo.save!
+#     p new_photo
+#   end
 
-  new_provider_category = ProviderCategory.new(category: Category.find_by(name: 'Fitness'), provider: new_provider)
-  new_provider_category.save!
+#   new_provider_category = ProviderCategory.new(category: Category.find_by(name: 'Fitness'), provider: new_provider)
+#   new_provider_category.save!
 
-  company['tags'].each do |tag|
-    new_tag = Tag.new(name: tag)
-    if !Tag.all.include?(new_tag)
-      new_tag.category = Category.find_by(name: 'Fitness')
-      new_tag.save!
-      new_provider_tag = ProviderTag.new(tag: new_tag, provider: new_provider)
-      new_provider_tag.save!
-      p new_provider_tag
-    else
-      new_provider_tag = ProviderTag.new(tag: Tag.find_by(name: tag), provider: new_provider)
-      new_provider_tag.save!
-      p new_provider_tag
-    end
-    new_provider.save!
-  end
-end
+#   company['tags'].each do |tag|
+#     new_tag = Tag.new(name: tag)
+#     if !Tag.all.include?(new_tag)
+#       new_tag.category = Category.find_by(name: 'Fitness')
+#       new_tag.save!
+#       new_provider_tag = ProviderTag.new(tag: new_tag, provider: new_provider)
+#       new_provider_tag.save!
+#       p new_provider_tag
+#     else
+#       new_provider_tag = ProviderTag.new(tag: Tag.find_by(name: tag), provider: new_provider)
+#       new_provider_tag.save!
+#       p new_provider_tag
+#     end
+#     new_provider.save!
+#   end
+# end
 
 
 
@@ -460,10 +460,33 @@ end
 #   end
 
 #   new_provider.save if Provider.find_by(name: row[:name]).nil?
-#   # counter += 1 if new_provider.save
-#   # break if counter > 10
+#   counter += 1 if new_provider.save
+#   break if counter > 10
 # end
 
+
+puts 'Editing restaurants'
+file_path = File.join(__dir__, 'restaurants.csv')
+CSV.foreach(file_path, headers: true, header_converters: :symbol) do |row|
+  if !Provider.find_by(name: row[:name]).nil?
+    provider = Provider.find_by(name: row[:name])
+    post_code = row[:address].match(/Singapore \((.*)\)/)[1]
+    url = "https://developers.onemap.sg/commonapi/search?searchVal=#{post_code}&returnGeom=Y&getAddrDetails=Y&pageNum=1"
+    response = open(url).read
+    results = JSON.parse(response)["results"][0]
+    if !results.empty?
+      street_address = results["ADDRESS"]
+      latitude = results["LATITUDE"]
+      longitude = results["LONGITUDE"]
+    end
+    provider.street_address = street_address
+    provider.latitude = latitude
+    provider.longitude = longitude
+
+    provider.save!
+    puts "#{provider.name}: #{provider.latitude} & #{provider.longitude}"
+  end
+end
 
 
 # # Seed users
